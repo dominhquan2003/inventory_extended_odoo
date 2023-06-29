0a6d6c2fbdd371f37c61e44ebf43f68674186313
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2023-06-29 20:20:20 +0700

    đơn hàng mới

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,7 +1,8 @@
-from odoo import models, fields, api  # type:ignore
+from odoo import models, fields, api, _  # type:ignore
 
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-
+    name = fields.Char(
+        default=lambda self: _('Đơn hàng mới'))
