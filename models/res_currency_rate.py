918316160e97344e6f356a39df55f55e9b1ae507
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2023-07-25 13:40:43 +0700

    bỏ ràng buộc

--- models/res_currency_rate.py
+++ models/res_currency_rate.py
@@ -4,9 +4,15 @@ from odoo.exceptions import ValidationError  # type:ignore
 
 class ResCurrencyRate(models.Model):
     _inherit = "res.currency.rate"
+    _order = "write_date desc"
 
     currency_id = fields.Many2one(
         default=lambda self: (
             self.env.ref('base.VND').id
         )
     )
+
+    _sql_constraints = [
+        ('unique_name_per_day', 'Check(1=1)', 'Only one currency rate per day allowed!'),
+        ('currency_rate_check', 'CHECK (rate>0)', 'The currency rate must be strictly positive.'),
+    ]
