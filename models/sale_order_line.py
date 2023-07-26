ffe2aea26d3cacbe382e956601963e359a5ec61a
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2023-07-26 21:25:45 +0700

    ẩn cái field khốn nạn price_unit

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -1,17 +1,36 @@
 from odoo import models, fields, api  # type:ignore
 from odoo.exceptions import ValidationError  # type:ignore
+import logging
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    # order_id = fields.Many2one('sale.order', string='Sale Order', related='order_id', readonly=True)
+  
+
     partner_id_test = fields.Many2one(
         "res.partner",
         related='order_id.partner_id',
     )
     virtual_available = fields.Float(
-        related="product_template_id.virtual_available")
-    qty_available = fields.Float(related="product_template_id.qty_available")
+        related="product_template_id.virtual_available"
+    )
+    qty_available = fields.Float(
+        related="product_template_id.qty_available"
+    )
+     # kiểm tra xem user hiện tại có ở trong group 'sales_team.group_sale_manager' không
+    sale_group = fields.Boolean(
+        compute="_compute_sale_group",
+    )
+
+    def _compute_sale_group(self):
+        is_admin = self.env.user in self.env.ref('sales_team.group_sale_manager').users
+        # context = dict(self.env.context)
+        # context.update({'is_admin': 0})
+        # self.env.context = context
+        logging.error(f"AAAAAAAAAAAAAA{self.env.context}")
+        for rc in self:
+            rc.sale_group = is_admin
+            
 
     def name_get(self):
         result = []
@@ -19,3 +38,4 @@ class SaleOrderLine(models.Model):
             name = so_line.product_id.name
             result.append((so_line.id, name))
         return result
+    
